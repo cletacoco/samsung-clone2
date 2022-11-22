d3ada04cfe0491a5524e4bda9ceb3c6e022bb59b
cletacoco <cletacoco91@naver.com>
2022-11-22 11:15:40 +0900

    11-22

---
+++ js/header.js
@@ -0,0 +1,11 @@
+const headerNav = document.querySelector('.header-nav');
+const header = document.querySelector('header');
+
+headerNav.addEventListener('mouseenter', () =>{
+    header.classList.add('active');
+})
+
+header.addEventListener('mouseleave', () => {
+    header.classList.remove('active');
+})
+
